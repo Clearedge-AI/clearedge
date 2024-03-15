025a603c4a82addbba40e627c3032e3b614aa0d3
yash bhaskar <yash9439@gmail.com>
2024-03-15 11:18:11 +0530

    Remove redundant functions from PDF utils

--- clearedge/reader/pdf.py
+++ clearedge/reader/pdf.py
@@ -4,12 +4,10 @@ from pdf2image import convert_from_path
 from paddleocr import PaddleOCR
 from io import StringIO
 from clearedge.utils.pdf_utils import (
-  check_merge,
   check_pdf,
   clean_blocks,
   check_divide,
   get_token_list,
-  merge
 )
 import fitz
 import json
@@ -31,104 +29,100 @@ def process_file(filepath):
 def fast_pdf_parser(doc):
   page_wise_block_list = []
   block_list = []
-  line_gaps = {}
   output_json = []
   num_pages = doc.page_count
+  print("The number of pages are ", num_pages)
 
-  for page_no in tqdm(range(num_pages), desc=f"Extracting {doc.name}"):
-    page = doc.load_page(page_no)
+  for page_no in tqdm(range(num_pages), desc=f"Extracting {os.path.basename(doc.name)}"):
+      page = doc.load_page(page_no)
 
-    page_data = page.get_text("dict", flags=fitz.TEXT_INHIBIT_SPACES)
+      page_data = page.get_text("dict", flags=fitz.TEXT_INHIBIT_SPACES)
 
-    page_data["blocks"] = [
-      block for block in page_data["blocks"] if block["type"] == 0
-    ]
-    [block.update({'list_item_start': False}) for block in page_data["blocks"]]
+      page_data["blocks"] = [
+          block for block in page_data["blocks"] if block["type"] == 0]
+      [block.update({'list_item_start': False}) for block in page_data["blocks"]]
 
-    # initialize empty list
-    for block_no, block in enumerate(page_data["blocks"]):
-      for line_no, _ in enumerate(block["lines"]):
-        page_data["blocks"][block_no]["lines"][line_no]["tokens"] = []
-        page_data["blocks"][block_no]["lines"][line_no]["word_bbox"] = []
+      # initialize empty list
+      for block_no, block in enumerate(page_data["blocks"]):
+          for line_no, _ in enumerate(block["lines"]):
+              page_data["blocks"][block_no]["lines"][line_no]["tokens"] = []
+              page_data["blocks"][block_no]["lines"][line_no]["word_bbox"] = []
 
-    # Add word tokens and bbox to lines
-    word_data_list = page.get_text("words")
+      # Add word tokens and bbox to lines
+      word_data_list = page.get_text("words")
 
-    for word_data in word_data_list:
-      block_no = word_data[5]
-      line_no = word_data[6]
-      bbox = list(word_data[:4])
-      bbox[0] = bbox[0] / page_data["width"]
-      bbox[1] = bbox[1] / page_data["height"]
-      bbox[2] = bbox[2] / page_data["width"]
-      bbox[3] = bbox[3] / page_data["height"]
-      page_data["blocks"][block_no]["lines"][line_no]["tokens"].append(
-        word_data[4]
-      )
-      page_data["blocks"][block_no]["lines"][line_no]["word_bbox"].append(
-        tuple(bbox + [page_no])
-      )
+      for word_data in word_data_list:
+          block_no = word_data[5]
+          line_no = word_data[6]
+          bbox = list(word_data[:4])
+          bbox[0] = bbox[0] / page_data["width"]
+          bbox[1] = bbox[1] / page_data["height"]
+          bbox[2] = bbox[2] / page_data["width"]
+          bbox[3] = bbox[3] / page_data["height"]
+          page_data["blocks"][block_no]["lines"][line_no]["tokens"].append(
+              word_data[4])
+          page_data["blocks"][block_no]["lines"][line_no]["word_bbox"].append(
+              tuple(bbox + [page_no]))
+          
 
-    page_data["blocks"] = clean_blocks(page_data["blocks"])
-    divided_block_list = []
-    for block in page_data["blocks"]:
-      divided_block_list.extend(check_divide(block))
-    page_data["blocks"] = clean_blocks(divided_block_list)
-    page_wise_block_list.append(page_data["blocks"])
+      page_data["blocks"] = clean_blocks(page_data["blocks"])
+      divided_block_list = []
+      for block in page_data["blocks"]:
+          divided_block_list.extend(check_divide(block))
+      page_data["blocks"] = clean_blocks(divided_block_list)
+
+      page_wise_block_list.append(page_data["blocks"])
 
   for page_no, blocks in enumerate(page_wise_block_list):
-    curr_segment_list = [get_token_list(block) for block in blocks]
-    curr_page_content = '\n\n'.join([" ".join(segment["tokens"]) for segment in curr_segment_list])
-    bbox = []
-    individual_text = []
-    for block in blocks:
-      x1 = block['bbox'][0]
-      y1 = block['bbox'][1]
-      x2 = block['bbox'][2]
-      y2 = block['bbox'][3]
-      bbox.append({"top": y1, "left": x1, "width": x2 - x1, "height": y2 - y1})
-      individual_text.append(str(block['lines'][0]['spans'][0]['text']))
-    meta_data = {"title": "", "section_header": "", "page": page_no + 1, "bbox": bbox, "individual_text": individual_text}
-    output_json.append({"raw_text": curr_page_content, "metadata": meta_data})
+      curr_segment_list = [get_token_list(block) for block in blocks]
+      curr_page_content = '\n\n'.join([" ".join(segment["tokens"]) for segment in curr_segment_list])
+      bbox = []
+      individual_text = []
+      for block in blocks:
+          x1 = block['bbox'][0]
+          y1 = block['bbox'][1]
+          x2 = block['bbox'][2]
+          y2 = block['bbox'][3]
+          bbox.append({"top": y1, "left": x1, "width": x2-x1, "height": y2-y1})
+          individual_text.append(str(block['lines'][0]['spans'][0]['text']))
+      meta_data = {"title": "", "section_header": "", "page": page_no+1, "bbox": bbox, "individual_text": individual_text}
+      output_json.append({"raw_text": curr_page_content, "metadata": meta_data})
 
   for page_wise_blocks in page_wise_block_list:
-    block_list.extend(page_wise_blocks)
-
+      block_list.extend(page_wise_blocks)
+      
   if len(block_list) == 0:
-    return []
+      return []
 
-  block_list = [{**block, 'idx': idx} for idx, block in enumerate(block_list)]
+  # # Saving the text content
+  # os.makedirs("./test_output", exist_ok=True)
+  # with open(
+  #     os.path.join(
+  #         "./test_output", ".".join(os.path.basename(filepath).split(".")[:-1])+".txt"),
+  #     "w+",
+  # ) as f:
+  #     segment_list = [get_token_list(block) for block in block_list]
 
-  redemptions = 2
-  redemption_thresh = 3
+  #     print('\n\n'.join([" ".join(segment["tokens"])
+  #             for segment in segment_list]), file=f)
 
-  for _ in range(redemptions):
-    global block_miss
-    global block_double
+  # segment_list = [get_token_list(block) for block in block_list]
 
-    block_miss = defaultdict(list)
-    block_double = defaultdict(list)
+  # # Saving the JSON output
+  # # Creating the directory if it doesn't exist
+  # os.makedirs("./test_output", exist_ok=True)
 
-    merged_block_list = [block_list[0]]
+  # # Filepath of the JSON file you want to write
+  # filepath = "./test_output/{}.json".format(os.path.basename(filepath).split(".")[0])
 
-    for block in block_list[1:]:
-      can_merge = check_merge(merged_block_list[-1], block, line_gaps)
-      if can_merge:
-        merged_block_list[-1] = merge(merged_block_list[-1], block)
-      else:
-        merged_block_list.append(block)
-
-    block_list = merged_block_list
-
-    for span_hash_gap in block_double.keys():
-      if len(block_double[span_hash_gap]) > redemption_thresh:
-        span_hash, gap = span_hash_gap.split('~')
-        line_gaps[span_hash] = line_gaps[span_hash] if span_hash in line_gaps else []
-        line_gaps[span_hash].append(float(gap))
+  # # Writing JSON data to the file
+  # with open(filepath, "w+") as f:
+  #     json.dump(output_json, f, indent=4)
 
   segment_list = [get_token_list(block) for block in block_list]
-
+            
   return '\n\n'.join([" ".join(segment["tokens"]) for segment in segment_list]), output_json
+
 
 def pdf2img(pdf_path):
     # Remove existing JPEG files in the pages directory
